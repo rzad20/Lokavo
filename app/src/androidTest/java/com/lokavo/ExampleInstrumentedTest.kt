82b31f840ff481849ac73ea9dd2980c43f314139
KrisnaDiva <krisnadiva0412@gmail.com>
2024-05-20 16:49:26 +0700

    create register_activity

--- app/src/androidTest/java/com/lokavo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lokavo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.capstonebangkit.lokavo
+package com.lokavo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/com/lokavo/EmailEditText.kt
@@ -0,0 +1,19 @@
+package com.lokavo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Patterns
+import com.google.android.material.textfield.TextInputEditText
+
+class EmailEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : TextInputEditText(context, attrs) {
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        if (s.toString().isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+            setError(context.getString(R.string.email_error), null)
+        } else {
+            error = null
+        }
+    }
+}

174f60e37c0a099d06f9f3bd9851ac4b2156ac14
KrisnaDiva <krisnadiva0412@gmail.com>
2024-06-01 22:37:11 +0700

    fix marker disappearing when screen is rotated

--- app/src/main/java/com/lokavo/ui/result/ResultViewModel.kt
+++ app/src/main/java/com/lokavo/ui/result/ResultViewModel.kt
@@ -1,17 +1,32 @@
 package com.lokavo.ui.result
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.lokavo.data.repository.MapsRepository
 import com.lokavo.data.Result
 import com.lokavo.data.remote.response.PlacesItem
 
 class ResultViewModel(private val repository: MapsRepository) : ViewModel() {
+    private val _latLng = MutableLiveData<LatLng>(null)
+    val latLng: LiveData<LatLng> get() = _latLng
+    private val _markers = MutableLiveData<MutableList<Marker>>(null)
+    val markers: LiveData<MutableList<Marker>> get() = _markers
     fun getPlaces(latitude: Double, longitude: Double): LiveData<Result<List<PlacesItem>>?> {
         return repository.getNearbyPlace(latitude, longitude)
     }
+
     fun getPlaceDetail(placeId: String): LiveData<Result<PlacesItem>?> {
         return repository.getPlaceDetail(placeId)
     }
 
+    fun setLatLng(latLng: LatLng) {
+        _latLng.value = latLng
+    }
+
+    fun setMarkers(markers: MutableList<Marker>) {
+        _markers.value = markers
+    }
 }
